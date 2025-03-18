586ae0ee843a72694c32dbbfd84b89e94cbbf99a
hv-914 <harshath3018@gmail.com>
2025-03-18 07:50:11 +0530

    Improved backend, with return codes

--- home.py
+++ home.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 sketches = os.path.join(os.getcwd(), "sketches")
 os.makedirs(sketches, exist_ok = True)
 
-@app.route("/save_sketch", methods = ['POST'])
+@app.route("/save_sketch", methods = ["POST"])
 def save_sketch():
     data = request.json
     code = data.get("code", "")
 
     if not code.strip():
-        return jsonify({"error": "Code cannot be empty!"}), 400
-
-    file_path = os.path.join(sketches, "sketch.ino")
+        return jsonify({"error": "Code cannot be empty"}), 400
     
+    file_path = os.path.join(sketches, "sketch.ino")
+
     with open(file_path, "w") as f:
         f.write(code)
 
-    return jsonify({"message": "Sketch saved successfully!", "path": file_path})
+    return jsonify({"message": "Sketch saved successfully", "path": file_path}), 200
 
 @app.route("/")
 def homepage():
--- sketches/sketch.ino
+++ sketches/sketch.ino
@@ -1,7 +1,3 @@
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+import python as python
+import shit as shit
+import hola as vamos
